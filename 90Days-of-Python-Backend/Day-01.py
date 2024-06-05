d8ff24d28ff29f172d5811ee3e626e1380567929
bor <ferali.eh@gmail.com>
2024-06-05 13:41:00 -0600

    Day-1

---
+++ 90Days-of-Python-Backend/Day-01.py
@@ -0,0 +1,25 @@
+# Día 1: Introducción a Python 
+# Definición del tema:
+# Python es un lenguaje de programación de alto nivel, interpretado y de propósito general. 
+# Es conocido por su sintaxis sencilla y legibilidad, lo que lo hace ideal para principiantes.
+
+# Ejercicios:
+
+# Instala Python y ejecuta tu primer script que imprima "Hola, Mundo".
+print("hola, Me llamo Eric Hernandez Edwards")
+# Escribe un programa que solicite tu nombre y lo imprima.
+name = input("Mi nombre es: ")
+print(name)
+# Escribe un programa que solicite dos números y los sume.
+num1 = int(input("el numero1 es: "))
+num2 = int(input("el numero2 es: "))
+resultado = num1 + num2
+print("el resultado de la suma es:",resultado)
+########
+number1 = int(input("el numero 1 es:"))
+number2 = int(input("el numero 2 es:"))
+results = number1 - number2
+print("el resultado de la resta es:", results)
+
+
+
