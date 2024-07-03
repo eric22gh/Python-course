7fbcad28f7476727b36e23d1ff119ff9a4fc44ff
bor <ferali.eh@gmail.com>
2024-07-02 21:49:06 -0600

    hello

---
+++ holamundo/16-files-csv.py
@@ -0,0 +1,27 @@
+# import csv
+
+# with open("file\\datos.csv") as archivo:
+#     reader = csv.reader(archivo)
+#     for row in reader:
+#         print(row)
+
+import pandas as pd
+df = pd.read_csv("Datos.csv")
+#df = pd.read_csv("Datos.csv",names=["Name","Age","City"])
+print(df)
+print(df["Edad"])
+
+orden = df.sort_values("Ciudad")
+print(orden)
+
+orden_ascending = df.sort_values("Ciudad",ascending=False)
+print(orden_ascending)
+
+concatenando = pd.concat([df,df])
+print(concatenando)
+
+primera_fila = pd.head(0) 
+
+ultimas_filas = df.tail(2)
+
+filas,comlumnas = df.shape # da el numero de fika y columnas del file csv
