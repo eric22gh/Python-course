08923a298311619ce9ea7d92f1325db444a325ac
bor <ferali.eh@gmail.com>
2024-07-09 21:44:07 -0600

    hello

--- holamundo/15-files_manage.py
+++ holamundo/15-files_manage.py
@@ -1,2 +1,13 @@
 with open("fof\\tex.txt", "r", encoding="UTF-8") as te: # encoding="UTF-8" para los carateres raros
     te.read()
+
+nombres = ["lucas","alewx", "camila", "erwin"]
+apellidos = ["edwards", "lopez", "vargas","pertez"]
+
+##### registrar las listas en un archivo
+
+with open("nombresyapellidos.txt","w") as arch:
+    arch.writelines("los datos son:\n\n")
+    [arch.writelines(f"nombre: {n}\nApeliido: {a}\n------------\n") for n,a in zip(nombres,apellidos)]
+    #####
+    
