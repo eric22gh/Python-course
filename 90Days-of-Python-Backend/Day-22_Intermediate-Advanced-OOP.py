7a4d3e7aa260336433bf53eb9f33e10275e4d3dc
bor <ferali.eh@gmail.com>
2024-10-28 14:36:07 -0600

    Changes from Day-22

--- 90Days-of-Python-Backend/Day-22_Intermediate-Advanced-OOP.py
+++ 90Days-of-Python-Backend/Day-22_Intermediate-Advanced-OOP.py
@@ -64,7 +64,7 @@ class Rectangulo(Forma):
         self.alto = alto
 
     def area(self):
-        return self.ancho * self.alto
+        return self.alto * self.ancho
 
     def perimetro(self):
         return 2 * (self.ancho + self.alto)
@@ -187,10 +187,8 @@ carro.mostrar()
 
 # Ejercicio 2: Añade un método acelerar a la clase Coche que imprima un mensaje indicando que el coche está acelerando. 
 # Usa este método en los objetos creados en el ejercicio 1.
-
 #técnica conocida como "monkey patching".agregando métodos o modificando atributos existentes, aunque no es una buena practica es mejor añadir todos los metodos.
 # a la hora de construir la clase.
-
 def acelerar(self): # crear el metodo para la clase
     print(f"este carro: {self.marca} esta acelerando")
 Coche.acelerar = acelerar # añadir el metodo a la clase
@@ -224,7 +222,7 @@ class Estudiante(Persona):
         self.curso = curso
 
     def estudiar(self):
-        print(f"el estundiante: {self.nombre}, esta estudiando el curso: {self.curso}")
+        print(f"el estudiante: {self.nombre}, esta estudiando el curso: {self.curso}")
 
 materia = Estudiante("rodri", 45, "informatica")
 materia.estudiar()
@@ -324,7 +322,234 @@ class Gerente(Empleado):
         super().__init__(nombre, apellido, sueldo)
         self.departamento = departamento
     def impresion(self):
-        return f"Este empleado de nombre: {self.nombre} {self.apellido} es del depártamento de: {self.departemento}"
+        return f"Este empleado de nombre: {self.nombre} {self.apellido} es del depártamento de: {self.departamento}"
     
 empleo = Gerente("eric", "edwards", 500000, "ventas")
 print(empleo.impresion())
+
+# Ejercicio 11
+# Define una clase Libro con atributos titulo, autor y año_publicacion. Crea dos objetos de la clase y muestra sus atributos.
+class Libro():
+    def __init__(self, titulo, autor, año_publicacion):
+        self.titulo = titulo
+        self.autor = autor
+        self.año_publicacion = año_publicacion
+    
+    def informacion(self):
+        return f"El nombre del autor es: {self.autor} y el libro es: {self.titulo} y su año de publicacion es: {self.año_publicacion}"
+informacio1 = Libro("El Cuervo", "Gabriel Garcia", 1985)
+informacio2 = Libro("Sombras del pasado", "Eric Calvin", 1975)
+print(informacio1.informacion())
+print(informacio2.informacion())
+
+##################################
+class Libro:
+    def __init__(self, titulo, autor, año_publicacion):
+        self.titulo = titulo
+        self.autor = autor
+        self.año_publicacion = año_publicacion
+
+    def __str__(self):
+        return f"{self.titulo} de {self.autor}, publicado en {self.año_publicacion}"
+
+informacio1 = Libro("El Cuervo", "Gabriel Garcia", 1985)
+informacio2 = Libro("Sombras del pasado", "Eric Calvin", 1975)
+print(informacio1)
+print(informacio2)
+
+# Ejercicio 12
+# Añade un método resumir a la clase Libro que imprima un resumen del libro en el formato: "El libro 'titulo' fue escrito por autor en el año año_publicacion."
+def resumir(self):
+    return f"El libro {self.autor} fue escrito por autor en el año: {self.año_publicacion}"
+Libro.resumir = resumir # añadir este metodo a la clase anterior, sin embargo no es una buena practica
+print(informacio1.resumir())
+
+# Ejercicio 13
+# Define una clase Animal con un método hacer_sonido que imprima un sonido genérico. Luego, crea una clase Gato que herede de Animal y
+# sobrescriba el método hacer_sonido para que imprima "Miau".
+class Animal():
+    def __init__(self, animal):
+        self.animal = animal
+    def hacer_sonido(self):
+        return f"El animal hace un sonido"
+        
+class Gato(Animal):
+    def __init__(self, animal):
+        super().__init__(animal)
+    def hacer_sonido(self):
+        super().hacer_sonido()
+        return f"{super().hacer_sonido()} y es Miau"
+    
+info = Gato("Felix")
+print(info.hacer_sonido())
+
+# Ejercicio 14
+# Crea una clase Vehiculo con atributos marca, modelo y año. Define un método info que devuelva una cadena con la información del vehículo. 
+# Luego, crea una clase Camion que herede de Vehiculo y añada un atributo capacidad_carga.
+class Vehiculo():
+    def __init__(self, marca, modelo, año):
+        self.marca = marca
+        self.modelo = modelo
+        self.año = año
+
+    def informacion_vehiculo(self):
+        return f"Este vehiculo de marca: {self.marca}, modelo: {self.modelo} y del año: {self.año}"
+
+class Camion(Vehiculo):
+    def __init__(self, marca, modelo, año, capacidad_carga):
+        super().__init__(marca, modelo, año)
+        self.capacidad_carga = capacidad_carga
+
+    def informacion_vehiculo(self):
+        return f"{super().informacion_vehiculo()}, tiene una capacidad de carga de: {self.capacidad_carga} toneladas"
+    
+dato = Camion("Mac", "bost", 2001, 18000)
+print(dato.informacion_vehiculo())
+
+# Ejercicio 15
+# Define una clase Cuenta con atributos titular y saldo. Añade métodos para depositar, retirar y mostrar_saldo. 
+# Asegúrate de que no se pueda retirar más de lo que hay en el saldo.
+class Cuenta():
+    def __init__(self, Titular, Saldo):
+        self.Titular = Titular
+        self.Saldo = Saldo
+
+    def depositar(self, Monto):
+        self.Saldo += Monto 
+        return f"El nuevo saldo despues de el deposito es: {self.Saldo}"
+    
+    def retirar(self, Monto):
+        if Monto > self.Saldo:
+            return "El monto a retirar es mayor que el saldo en su cuenta, ingrese un monto valido"
+        else:
+            self.Saldo -= Monto
+            return f"El nuevo saldo despues de el retiro es: {self.Saldo}"
+    
+    def mostrar_saldo(self):
+        return f"El saldo en tu cuenta bancaria es de: {self.Saldo}"
+    
+dato = Cuenta("Eric Hernandez", 250000)
+print(dato.depositar(2500))
+print(dato.retirar(1000000))
+print(dato.mostrar_saldo())
+
+# Ejercicio 16
+# Crea una clase Producto con atributos nombre, precio y stock. Añade métodos para vender (disminuyendo el stock) y reabastecer (aumentando el stock).
+class Producto():
+    def __init__(self, nombre, precio, stock):
+        self.nombre = nombre
+        self.precio = precio
+        self.stock = stock
+
+    def vender(self, cantidad):
+        if cantidad > self.stock:
+            return "No hay suficiente stock para realizar la venta."
+        self.stock -= cantidad
+        return f"Después de la venta, el stock se ha disminuido y ahora es de: {self.stock}"
+    
+    def reabastecer(self, cantidad):
+        if cantidad <= 0:
+            return "La cantidad a reabastecer debe ser mayor que cero."
+        self.stock += cantidad
+        return f"El stock ha aumentado y ahora es de: {self.stock}"
+    
+dato = Producto("Taladro", 250000, 25)
+print(dato.vender(10))
+print(dato.reabastecer(2))
+
+# Ejercicio 17
+# Define una clase Curso que tenga atributos nombre, codigo y estudiantes. Añade un método para inscribir_estudiante que añada 
+# un estudiante a la lista de estudiantes.
+class Curso():
+    def __init__(self, nombre, codigo):
+        self.nombre = nombre
+        self.codigo = codigo
+        self.estudiantes = []
+    
+    def inscribir_estudiante(self, nombre_estudiante):
+        if nombre_estudiante.lower() in self.estudiantes: # para evitar el duplicado de nombres
+            return f"El estudiante {nombre_estudiante} ya está inscrito."
+        self.estudiantes.append(nombre_estudiante)
+        return F"esta es la lista de estudiantes: {self.estudiantes}"
+    
+dato = Curso("Ciencias", 21)
+print(dato.inscribir_estudiante("helen"))
+print(dato.inscribir_estudiante("alex"))
+print(dato.inscribir_estudiante("Helen"))
+
+# Ejercicio 18
+# Crea una clase Ordenador con atributos marca, modelo y ram. Define un método que imprima las especificaciones del ordenador. 
+# Luego, crea una subclase Portatil que añada un atributo peso.
+class Ordenador():
+    def __init__(self, marca, modelo, ram):
+        if ram <= 0:
+            raise ValueError("La RAM debe ser un número positivo.")
+        self.marca = marca
+        self.modelo = modelo
+        self.ram = ram
+    
+    def especificaciones(self):
+        return f"Este ordenador de marca: {self.marca}, modelo: {self.modelo}, tiene una memoria ram de: {self.ram} GB"
+
+class Portatil(Ordenador):
+    def __init__(self, marca, modelo, ram, peso):
+        super().__init__(marca, modelo, ram)
+        if peso <= 0:
+            raise ValueError("El peso debe ser un número positivo.")
+        self.peso = peso
+
+    def especificaciones(self):
+        return F"{super().especificaciones()} y tambien tiene un peso de: {self.peso} gramos"
+    
+dato = Ordenador("HP", "LX", 18)
+dato2 = Portatil("HP", "LX", 18, 1000)
+print(dato.especificaciones())
+print(dato2.especificaciones())
+
+# Ejercicio 19
+# Define una clase Empleado con atributos nombre, apellido y sueldo. Crea un método que calcule el sueldo anual. 
+# Luego, crea una clase Vendedor que herede de Empleado y añada un atributo comision.
+class Empleado():
+    def __init__(self, nombre, apellido, sueldo):
+        if sueldo < 0:
+            raise ValueError("El sueldo debe ser un número positivo.")
+        self.nombre = nombre
+        self.apellido = apellido
+        self.sueldo = sueldo
+
+    def sueldo_anual(self):
+        nuevo_sueldo = self.sueldo * 12
+        return f"El empleado: {self.nombre} {self.apellido} tiene un sueldo añual de: {nuevo_sueldo:.0f} colones"
+
+class Vendedor(Empleado):
+    def __init__(self, nombre, apellido, sueldo, comision):
+        super().__init__(nombre, apellido, sueldo)
+        if comision < 0:
+            raise ValueError("La comisión debe ser un número positivo.")
+        self.comision = comision
+
+    def sueldo_anual(self):
+        sueldo_anual_base = self.sueldo * 12
+        sueldo_total = sueldo_anual_base + (sueldo_anual_base * self.comision / 100)
+        return f"{super().sueldo_anual()} más una comisión del: {self.comision}% que totaliza: {sueldo_total:.0f} colones"
+    
+dato = Vendedor("Eric", "Calvin", 850000, 10)
+print(dato.sueldo_anual())
+
+# Ejercicio 20
+# Crea una clase Juego con un método iniciar. Luego, define una clase JuegoDeMesa que herede de Juego 
+# y sobrescriba el método iniciar para incluir un mensaje que indique que el juego de mesa ha comenzado.
+class Juego():
+    def __init__(self, juego):
+        self.juego = juego
+    def Iniciar(self):
+        pass
+class JuegoDeMesa(Juego):
+    def __init__(self, juego):
+        super().__init__(juego)
+
+    def Iniciar(self):
+        return f"El juego de mesa {self.juego} ha comenzado."
+    
+dato = JuegoDeMesa("domino")
+print(dato.Iniciar())
