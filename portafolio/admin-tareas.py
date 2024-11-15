8eb9fa51f87aacd998822e7b49ca95885efed3d1
bor <ferali.eh@gmail.com>
2024-11-15 12:09:58 -0600

    Day-29

--- portafolio/admin-tareas.py
+++ portafolio/admin-tareas.py
@@ -7,12 +7,22 @@ admin = admin_tareas.Tareas([])
 # ventana principal de Tkinter
 root = tk.Tk()
 root.title("Administrador de tareas")
+root.geometry("300x500")
 
 # Crear y agregar widgets a la ventana
 label = tk.Label(root, text="Administrador de tareas")
-label.pack()
+label.pack(pady=100)
 
-button_agregar = tk.Button(root, text="Agregar tarea", command=lambda: print(admin.agregar_tareas("Nueva tarea")))
+entrada = tk.Entry(root)
+entrada.pack()
+
+def agregar_tarea():
+    tarea = entrada.get() 
+    resultado = admin.agregar_tareas(tarea) 
+    print(resultado)
+    entrada.delete(0, tk.END)
+
+button_agregar = tk.Button(root, text="Agregar tarea", command=agregar_tarea)
 button_agregar.pack()
 
 button_eliminar = tk.Button(root, text="Eliminar tarea", command=lambda: print(admin.eliminar_tarea("Tarea eliminada")))
@@ -21,9 +31,11 @@ button_eliminar.pack()
 button_mostrar = tk.Button(root, text="Mostrar tareas", command=lambda: print(admin.mostrar_tareas()))
 button_mostrar.pack()
 
-button_salir = tk.Button(root, text="Salir", command=root.quit)
+button_salir = tk.Button(root, text="Salir", command=lambda: print(root.quit()))
 button_salir.pack()
 
 # Iniciar la interfaz gráfica
 root.mainloop()
 
+# averiguar por que se ejecuta el bucle primero
+# que se muestren las tareas en un parte abajo
